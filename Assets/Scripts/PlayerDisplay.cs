592a7f44d30662c21f5603d73d6be635f9772e9f
Jonathan Dervin <jdervin@ltu.edu>
2021-03-22 13:34:21 -0400

    Made Player UI

---
+++ Assets/Scripts/PlayerDisplay.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDisplay : MonoBehaviour
+{
+    public Text CurrencyDisplay;
+    public Transform healthParent;
+    public Transform healthIconPrefab;
+    public PlayerScript player;
+
+    private int healthDisplayed = 0;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+      
+        //CurrencyDisplay.text = CurrencyScript.Instance.balance.ToString();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (healthDisplayed != player.HP)
+        {
+            healthDisplayed = player.HP;
+            DisplayHealth();
+
+        }
+        CurrencyDisplay.text = CurrencyScript.Instance.balance.ToString();
+    }
+
+    void DisplayHealth()
+    {
+        Transform[] spawnedCrosses = healthParent.GetComponentsInChildren<Transform>();
+
+        for (int n = spawnedCrosses.Length-1; n>0; n--)
+        {
+            Destroy(spawnedCrosses[n].gameObject);
+        }
+
+
+        for (int i=0; i<healthDisplayed;i++)
+        {
+            Instantiate(healthIconPrefab, healthParent);
+        }
+    }
+}
