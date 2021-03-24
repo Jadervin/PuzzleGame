4b2a2e8a04abd3bc34ecb080951ee4548f69fc41
Jonathan Dervin <jdervin@ltu.edu>
2021-03-24 13:18:47 -0400

    Fixed the box Pushing Mechanic

--- Assets/Scripts/BoxScript.cs
+++ Assets/Scripts/BoxScript.cs
@@ -4,23 +4,45 @@ using UnityEngine;
 
 public class BoxScript : MonoBehaviour
 {
-    public bool isPushed;
-    public PlayerScript player;
+    public Transform player;
+    public bool isParented = false;
+    Vector3 lastPlayerPosition;
+    Transform originalParent;
 
-    // Start is called before the first frame update
-
-    private void OnCollisionEnter(Collision collision)
+    private void Start()
     {
-        if (collision.collider.gameObject.tag == "Player" && TryGetComponent<PlayerScript>(out player))
+        originalParent = transform.parent;
+    }
+
+    private void Update()
+    {
+        if (isParented)
         {
-            if((collision.collider.transform.position-player.lastPosition).normalized==
-                this.transform.position-collision.collider.transform.position)
+            Vector3 movementVec = player.position - lastPlayerPosition;
+            Vector3 directionVec = transform.position - player.position;
+
+            if (movementVec.x * directionVec.x < 0
+                || movementVec.z * directionVec.z < 0)
             {
-                
-                this.transform.SetParent(player.transform);
+                isParented = false;
+                transform.SetParent(originalParent);
             }
 
+
+            lastPlayerPosition = player.position;
         }
-        
+    }
+
+    //The player must have a triger around them. Make sure its big enough so the 
+    //skin width of the character controller doesnt interfere.
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (!isParented && collider.CompareTag("Player"))
+        {
+            player = collider.transform;
+            transform.SetParent(player);
+            isParented = true;
+            lastPlayerPosition = player.position;
+        }
     }
 }
