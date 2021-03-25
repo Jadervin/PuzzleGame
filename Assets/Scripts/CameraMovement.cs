2461788a946c72edc21d107fbefee36a505c4980
Jonathan Dervin <jdervin@ltu.edu>
2021-03-25 19:56:50 -0400

    Implemented Camera Movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public Transform target;
+    int degrees = 10;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(1))
+        {
+
+            transform.RotateAround(target.position, Vector3.up, Input.GetAxis("Mouse X") * degrees);
+            //transform.RotateAround (target.position, Vector3.left, Input.GetAxis ("Mouse Y")* degrees);
+        }
+        if (!Input.GetMouseButton(1))
+        {
+            //transform.RotateAround(target.position, Vector3.up, degrees * Time.deltaTime);
+        }
     }
+
 }
