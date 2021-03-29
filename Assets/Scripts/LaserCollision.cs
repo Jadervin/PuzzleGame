56c26afa9e7c4f3264d85914d29601c90ff0a6c9
Jonathan Dervin <jdervin@ltu.edu>
2021-03-28 22:26:05 -0400

    Laser Sound

--- Assets/Scripts/LaserCollision.cs
+++ Assets/Scripts/LaserCollision.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LaserCollision : MonoBehaviour
 {
+    public string youLose;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,20 +21,27 @@ public class LaserCollision : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == ("Alien"))
+        if (collision.gameObject.tag == ("Player"))
         {
             Destroy(collision.gameObject);
             //Instantiate(gitsEffect, transform.position, Quaternion.identity);
+            SceneManager.LoadScene(youLose);
 
-            
         }
 
-        if (collision.gameObject.tag == ("Tank"))
+        if (collision.gameObject.tag == "blue crate")
         {
-            Destroy(collision.gameObject);
-            //Instantiate(gitsEffect, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
 
-            //Destroy(this.gameObject);
+        if (collision.gameObject.tag == "red crate")
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (collision.gameObject.tag == "green crate")
+        {
+            Destroy(this.gameObject);
         }
 
     }
