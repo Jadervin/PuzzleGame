3be670ff3c94840f0f25725df4106fff1ce9db07
Jonathan Dervin <jdervin@ltu.edu>
2021-03-28 22:18:29 -0400

    Statue Eyes

---
+++ Assets/Scripts/LaserScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour
+{
+    public ParticleSystem[] laser;
+    //public ParticleSystem MuzzleFlash;
+    public int projectileSelected = 0;
+    public GameObject muzzle;
+    public float cooldownTime;
+    float coolTimer = 0;
+
+    public AudioSource soundSource;
+    public AudioClip laserSound;
+
+
+    private void Update()
+    {
+
+        //ChangeWeapons();
+        if (coolTimer > 0)
+        {
+            coolTimer -= Time.deltaTime;
+        }
+
+
+        else if (coolTimer <= 0)
+        {
+            Shoot();
+            coolTimer = cooldownTime;
+        }
+
+    }
+
+    void Shoot()
+    {
+        ParticleSystem temp;
+        //MuzzleFlash.Play();
+        temp = Instantiate(laser[projectileSelected], muzzle.transform.position,
+           muzzle.transform.rotation);
+        soundSource.PlayOneShot(laserSound);
+
+    }
+}
